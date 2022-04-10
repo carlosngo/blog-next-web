e54417d33f2374f69012a91785d4056cf1aa43d5
Carlos <carlos_ngo@dlsu.edu.ph>
2022-04-11 02:07:05 +0800

    inject mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import {MantineProvider} from "@mantine/core";
+import {NotificationsProvider} from '@mantine/notifications';
+
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: 'light',
+          }}>
+        <NotificationsProvider>
+          <Component {...pageProps} />
+        </NotificationsProvider>
+      </MantineProvider>
+  );
 }
 
 export default MyApp
